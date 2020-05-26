f0b6ac3da0d91e22924c85fc6276367961a2ac3d
micheledaddetta1 <micheledaddetta1@studenti.polito.it>
2020-05-26 20:53:06 +0200

    Prova

--- examples/applications/basic_embedding.py
+++ examples/applications/basic_embedding.py
@@ -3,7 +3,7 @@ This basic example loads a pre-trained model from the web and uses it to
 generate sentence embeddings for a given list of sentences.
 """
 
-from sentence_transformers import SentenceTransformer, LoggingHandler
+from CycleNLPGAN.sentence_transformers import SentenceTransformer, LoggingHandler
 import numpy as np
 import logging
 
