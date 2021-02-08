19854ec3e64bdb6f64fad88eed3aad1493b9d0ca
micheledaddetta1 <micheledaddetta1@studenti.polito.it>
2021-02-08 20:40:58 +0100

    Starting BUCC implementation

--- models/EncDecModel.py
+++ models/EncDecModel.py
@@ -41,14 +41,16 @@ class EncDecModel(nn.Module):
 
         self.add_pooling_layer()
 
-    def forward(self, sentences, target_sentences, partial_value=False):
+    def forward(self, sentences, target_sentences=None, partial_value=False):
 
         embeddings = self.batch_encode_plus(sentences, padding=True, verbose=False)
         embeddings = embeddings.to(self.model.device)
         if self.task == "translation":
-            decoder_input_ids = self.batch_encode_plus(target_sentences,  padding=True, verbose=False).input_ids.to(self.model.device)  # Batch size 1
-            outputs = self.model(input_ids=embeddings.input_ids, labels=decoder_input_ids, return_dict=True)
-
+            if target_sentences is not None:
+                decoder_input_ids = self.batch_encode_plus(target_sentences,  padding=True, verbose=False).input_ids.to(self.model.device)  # Batch size 1
+                outputs = self.model(input_ids=embeddings.input_ids, labels=decoder_input_ids, return_dict=True)
+            else:
+                outputs = self.model(input_ids=embeddings.input_ids, return_dict=True)
             output_sentences = self.model.generate(**embeddings)
             output_sentences = self.decode(output_sentences)
         else:
@@ -62,13 +64,22 @@ class EncDecModel(nn.Module):
             for i in range(len(sentences)):
                 a = outputs.encoder_last_hidden_state[i]
                 sentence_embedding[i] = self.embedding_pooling(outputs.encoder_last_hidden_state[i])["sentence_embedding"]
-            return output_sentences, sentence_embedding, outputs.loss
+            if target_sentences is not None:
+                return output_sentences, sentence_embedding, outputs.loss
+            else:
+                return output_sentences, sentence_embedding
         else:
             del embeddings
 
-            return output_sentences, outputs.loss
+            if target_sentences is not None:
+                return output_sentences, outputs.loss
+            else:
+                return output_sentences
 
     def get_word_embedding_dimension(self) -> int:
+        return self.config.hidden_size
+
+    def get_sentence_embedding_dimension(self) -> int:
         return self.config.hidden_size
 
     def tokenize(self, text: str) -> List[int]:
@@ -165,50 +176,100 @@ class EncDecModel(nn.Module):
         #self.config.hidden_size = self.model.base_model.encoder.config.hidden_size
         self.config.encoder_layers = self.model.base_model.encoder.config.num_hidden_layers
 
+    def encode(self, sentences: List[str], batch_size: int = 8, show_progress_bar: bool = None,
+               output_value: str = 'sentence_embedding', convert_to_numpy: bool = True) -> List[np.ndarray]:
+        """
+        Computes sentence embeddings
 
-    def encode(self, sentences, verbose=True):
-        train_input_ids = []
-        if verbose:
-            for text in tqdm(sentences):
-                input_ids = self.tokenizer.encode(
-                    text,
-                    return_tensors='pt',
-                    max_length=self.max_seq_length,
-                    padding=True,
-                    #pad_to_max_length=True,
-                    truncation=True
-                )
-                train_input_ids.append(input_ids)
-        else:
-            for text in sentences:
-                input_ids = self.tokenizer.encode(
-                    text,
-                    return_tensors='pt',
-                    max_length=self.max_seq_length,
-                    padding=True,
-                    #pad_to_max_length=True,
-                    truncation=True
-                )
+        :param sentences:
+           the sentences to embed
+        :param batch_size:
+           the batch size used for the computation
+        :param show_progress_bar:
+            Output a progress bar when encode sentences
+        :param output_value:
+            Default sentence_embedding, to get sentence embeddings. Can be set to token_embeddings
+            to get wordpiece token embeddings.
+        :param convert_to_numpy:
+            If true, the output is a list of numpy vectors. Else, it is a list of pytorch tensors.
+        :return:
+           Depending on convert_to_numpy, either a list of numpy vectors or a list of pytorch tensors
+        """
+        self.eval()
+        if show_progress_bar is None:
+            show_progress_bar = (
+                        logging.getLogger().getEffectiveLevel() == logging.INFO or logging.getLogger().getEffectiveLevel() == logging.DEBUG)
 
-                train_input_ids.append(input_ids)
+        all_embeddings = []
+        length_sorted_idx = np.argsort([len(sen) for sen in sentences])
 
-        train_input_ids = torch.cat(train_input_ids, dim=0)
-        return train_input_ids
+        iterator = range(0, len(sentences), batch_size)
+        if show_progress_bar:
+            iterator = tqdm(iterator, desc="Batches")
+
+        for batch_idx in iterator:
+            batch_tokens = []
+
+            batch_start = batch_idx
+            batch_end = min(batch_start + batch_size, len(sentences))
+
+            longest_seq = 0
+
+            for idx in length_sorted_idx[batch_start: batch_end]:
+                sentence = sentences[idx]
+                tokens = self.tokenize(sentence)
+                longest_seq = max(longest_seq, len(tokens))
+                batch_tokens.append(tokens)
+
+            features = {}
+            for text in batch_tokens:
+                sentence_features = self.get_sentence_features(text, longest_seq)
+
+                for feature_name in sentence_features:
+                    if feature_name not in features:
+                        features[feature_name] = []
+                    features[feature_name].append(sentence_features[feature_name])
+
+            for feature_name in features:
+                # features[feature_name] = torch.tensor(np.asarray(features[feature_name])).to(self.device)
+                features[feature_name] = torch.cat(features[feature_name]).to(self.model.device)
+
+            with torch.no_grad():
+                _, out_features = self.forward(features, partial_value=True)
+                embeddings = out_features
+
+                if output_value == 'token_embeddings':
+                    # Set token embeddings to 0 for padding tokens
+                    input_mask = out_features['attention_mask']
+                    input_mask_expanded = input_mask.unsqueeze(-1).expand(embeddings.size()).float()
+                    embeddings = embeddings * input_mask_expanded
+
+                if convert_to_numpy:
+                    embeddings = embeddings.to('cpu').numpy()
+
+                all_embeddings.extend(embeddings)
+
+        reverting_order = np.argsort(length_sorted_idx)
+        all_embeddings = [all_embeddings[idx] for idx in reverting_order]
+
+        return all_embeddings
+
 
     def batch_encode_plus(self, sentences, padding='max_length', verbose=True):
         train_input_ids = self.tokenizer.batch_encode_plus(
-                sentences,
-                return_tensors='pt',
-                max_length=self.max_seq_length,
-                padding=padding,
-                #pad_to_max_length=True,
-                truncation=True
-            )
+                    sentences,
+                    return_tensors='pt',
+                    max_length=self.max_seq_length,
+                    padding=padding,
+                    #pad_to_max_length=True,
+                    truncation=True,
+                )
         return train_input_ids
 
     def add_pooling_layer(self):
         if not hasattr(self, 'embedding_pooling'):# and self.task == "reconstruction":
             self.embedding_pooling = Pooling(self.get_word_embedding_dimension(),
-                                     pooling_mode_mean_tokens=True,
-                                     pooling_mode_cls_token=False,
-                                     pooling_mode_max_tokens=False)
+                                 pooling_mode_mean_tokens=True,
+                                 pooling_mode_cls_token=False,
+                                 pooling_mode_max_tokens=False)
+
