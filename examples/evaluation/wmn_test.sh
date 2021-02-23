cce2de2fa09855386504500bd7d5011610f78d9a
micheledaddetta1 <micheledaddetta1@studenti.polito.it>
2021-02-23 22:00:20 +0100

    Fix loading bash

--- examples/evaluation/wmn_test.sh
+++ examples/evaluation/wmn_test.sh
@@ -18,8 +18,7 @@ set -e
 
 BASE_DIR=$(pwd)
 
-OUTPUT_DIR=${OUTPUT_DIR:-$BASE_DIR/wmt14/de_en}
-echo "Writing to ${OUTPUT_DIR}. To change this, set the OUTPUT_DIR environment variable."
+OUTPUT_DIR=${OUTPUT_DIR:-$BASE_DIR/wmt14}
 
 OUTPUT_DIR_DATA="${OUTPUT_DIR}/data"
 
@@ -40,25 +39,52 @@ if [ ! -d "${OUTPUT_DIR}/mosesdecoder" ]; then
   git clone https://github.com/moses-smt/mosesdecoder.git "${OUTPUT_DIR}/mosesdecoder"
 fi
 
+mkdir ${OUTPUT_DIR_DATA}/de_en
+mkdir ${OUTPUT_DIR_DATA}/fr_en
+mkdir ${OUTPUT_DIR_DATA}/de_en/test
+mkdir ${OUTPUT_DIR_DATA}/fr_en/test
+
 # Convert SGM files
 # Convert newstest2014 data into raw text format
 ${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
   < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-src.de.sgm \
-  > ${OUTPUT_DIR_DATA}/test/newstest2014.src.de
+  > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.src.de
 ${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
   < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-ref.en.sgm \
-  > ${OUTPUT_DIR_DATA}/test/newstest2014.ref.en
+  > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.ref.en
 ${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
   < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-src.en.sgm \
-  > ${OUTPUT_DIR_DATA}/test/newstest2014.src.en
+  > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.src.en
 ${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
   < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-ref.de.sgm \
-  > ${OUTPUT_DIR_DATA}/test/newstest2014.ref.de
+  > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.ref.de
+
+
+mkdir ${OUTPUT_DIR}/de_en
+mkdir ${OUTPUT_DIR}/fr_en
+# Copy dev/test data to output dir
+cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.de ${OUTPUT_DIR}/de_en
+cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.en ${OUTPUT_DIR}/de_en
+
+
+# Convert newstest2014 data into raw text format
+${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-src.fr.sgm \
+  > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.src.fr
+${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-ref.en.sgm \
+  > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.ref.en
+${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-src.en.sgm \
+  > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.src.en
+${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-ref.fr.sgm \
+  > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.ref.fr
 
 
 # Copy dev/test data to output dir
-cp ${OUTPUT_DIR_DATA}/test/newstest20*.de ${OUTPUT_DIR}
-cp ${OUTPUT_DIR_DATA}/test/newstest20*.en ${OUTPUT_DIR}
+cp ${OUTPUT_DIR_DATA}/fr_en/test/newstest20*.fr ${OUTPUT_DIR}/fr_en
+cp ${OUTPUT_DIR_DATA}/fr_en/test/newstest20*.en ${OUTPUT_DIR}/fr_en
 
 rm -rf ${OUTPUT_DIR_DATA}/test
 rm -f ${OUTPUT_DIR_DATA}/test.tgz
