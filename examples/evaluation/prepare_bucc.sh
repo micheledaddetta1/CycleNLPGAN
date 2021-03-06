75c5ff332e33ab2065aea617abae41ca160a1eb9
MorenoLaQuatra <moreno.laquatra@gmail.com>
2021-03-06 10:12:17 +0000

    added prepare bucc

---
+++ examples/evaluation/prepare_bucc.sh
@@ -0,0 +1,17 @@
+wget https://comparable.limsi.fr/bucc2018/bucc2018-de-en.training-gold.tar.bz2
+wget https://comparable.limsi.fr/bucc2018/bucc2018-fr-en.training-gold.tar.bz2
+wget https://comparable.limsi.fr/bucc2018/bucc2018-ru-en.training-gold.tar.bz2
+wget https://comparable.limsi.fr/bucc2018/bucc2018-zh-en.training-gold.tar.bz2
+wget https://comparable.limsi.fr/bucc2018/bucc2018-de-en.sample-gold.tar.bz2
+wget https://comparable.limsi.fr/bucc2018/bucc2018-fr-en.sample-gold.tar.bz2
+wget https://comparable.limsi.fr/bucc2018/bucc2018-ru-en.sample-gold.tar.bz2
+wget https://comparable.limsi.fr/bucc2018/bucc2018-zh-en.sample-gold.tar.bz2
+
+tar xvf bucc2018-de-en.training-gold.tar.bz2
+tar xvf bucc2018-fr-en.training-gold.tar.bz2
+tar xvf bucc2018-ru-en.training-gold.tar.bz2
+tar xvf bucc2018-zh-en.training-gold.tar.bz2
+tar xvf bucc2018-de-en.sample-gold.tar.bz2
+tar xvf bucc2018-fr-en.sample-gold.tar.bz2
+tar xvf bucc2018-ru-en.sample-gold.tar.bz2
+tar xvf bucc2018-zh-en.sample-gold.tar.bz2
