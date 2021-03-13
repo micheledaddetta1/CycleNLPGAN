43b24c3d1aabf8f9d872593ddb5c5a2557f5b893
micheledaddetta1 <micheledaddetta1@studenti.polito.it>
2021-03-13 18:29:44 +0100

    ADD WMT 15,16,17

--- examples/evaluation/sacrebleu_test.py
+++ examples/evaluation/sacrebleu_test.py
@@ -13,7 +13,7 @@ import numpy as np
 batch_size = 32
 
 parser = argparse.ArgumentParser(description='')
-parser.add_argument('--language', required=True, help='Language compared to English')
+parser.add_argument('--language', required=True, help='Language compared to English [fr|de|zh|ru]')
 parser.add_argument('--models_dir', type=str, default='../../checkpoints/translation',
                     help='Root directory of saved models')
 parser.add_argument('--models_prefix', type=str, default='latest',
@@ -35,74 +35,82 @@ modelB = EncDecModel(modelB_path).to("cuda:0")
 
 language = args.language
 # Intput files for BUCC2018 shared task
-source_file = "wmt14/" + language + "_en/newstest2014.src." + language
-reference_file = "wmt14/" + language + "_en/newstest2014.ref.en"
 
-# metric = load_metric("sacrebleu")
-
-print("Read "+language+" source file")
-source_data = []
-with open(source_file, encoding='utf8') as fIn:
-    for line in fIn:
-        source_data.append(line)
-
-print("Read en reference file")
-reference_data = []
-with open(reference_file, encoding='utf8') as fIn:
-    for line in fIn:
-        reference_data.append(line)
-
-source_dataloader = DataLoader(
-    source_data,
-    batch_size=batch_size,
-    shuffle=False,
-    num_workers=1)
-
-print("Translate source data")
-translated_source_data = []
-for i, source_batch in tqdm(enumerate(source_dataloader), total=len(source_dataloader)):
-    model_prediction = modelA(source_batch)
-    translated_source_data.extend(model_prediction)
-
-bleu = sacrebleu.raw_corpus_bleu(translated_source_data, [reference_data]).score
+wmt_list = ["14", "15", "16", "17"]
+for wmt in wmt_list:
+    source_file = "wmt"+wmt+"/" + language + "_en/newstest20"+wmt+".src." + language
+    reference_file = "wmt"+wmt+"/" + language + "_en/newstest20"+wmt+".ref.en"
 
 
-print(language+"-en BLEU score: " + str(bleu))
+    print("Check "+language+" source file")
+    source_data = []
+    if not os.path.exists(source_file):
+        print("No data in WMT"+wmt+" for language " + language)
+        continue
+
+    print("Read "+language+" source file")
+    source_data = []
+    with open(source_file, encoding='utf8') as fIn:
+        for line in fIn:
+            source_data.append(line)
+
+    print("Read en reference file")
+    reference_data = []
+    with open(reference_file, encoding='utf8') as fIn:
+        for line in fIn:
+            reference_data.append(line)
+
+    source_dataloader = DataLoader(
+        source_data,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=1)
+
+    print("Translate source data")
+    translated_source_data = []
+    for i, source_batch in tqdm(enumerate(source_dataloader), total=len(source_dataloader)):
+        model_prediction = modelA(source_batch)
+        translated_source_data.extend(model_prediction)
+
+    bleu = sacrebleu.raw_corpus_bleu(translated_source_data, [reference_data]).score
+
+
+    print(language+"-en BLEU score WMT"+str(wmt)+": " + str(bleu))
+    
 
 
 
 
 
+    source_file = "wmt14/" + language + "_en/newstest2014.src.en"
+    reference_file = "wmt14/" + language + "_en/newstest2014.ref."+language
 
-source_file = "wmt14/" + language + "_en/newstest2014.src.en"
-reference_file = "wmt14/" + language + "_en/newstest2014.ref."+language
+    # metric = load_metric("sacrebleu")
 
-# metric = load_metric("sacrebleu")
+    print("Read en source file")
+    source_data = []
+    with open(source_file, encoding='utf8') as fIn:
+        for line in fIn:
+            source_data.append(line)
 
-print("Read en source file")
-source_data = []
-with open(source_file, encoding='utf8') as fIn:
-    for line in fIn:
-        source_data.append(line)
+    print("Read "+language+" reference file")
+    reference_data = []
+    with open(reference_file, encoding='utf8') as fIn:
+        for line in fIn:
+            reference_data.append(line)
 
-print("Read "+language+" reference file")
-reference_data = []
-with open(reference_file, encoding='utf8') as fIn:
-    for line in fIn:
-        reference_data.append(line)
+    source_dataloader = DataLoader(
+        source_data,
+        batch_size=batch_size,
+        shuffle=False,
+        num_workers=1)
 
-source_dataloader = DataLoader(
-    source_data,
-    batch_size=batch_size,
-    shuffle=False,
-    num_workers=1)
+    print("Translate source data")
+    translated_source_data = []
+    for i, source_batch in tqdm(enumerate(source_dataloader), total=len(source_dataloader)):
+        model_prediction = modelB(source_batch)
+        translated_source_data.extend(model_prediction)
 
-print("Translate source data")
-translated_source_data = []
-for i, source_batch in tqdm(enumerate(source_dataloader), total=len(source_dataloader)):
-    model_prediction = modelB(source_batch)
-    translated_source_data.extend(model_prediction)
+    bleu = sacrebleu.raw_corpus_bleu(translated_source_data, [reference_data]).score
 
-bleu = sacrebleu.raw_corpus_bleu(translated_source_data, [reference_data]).score
-
-print("en-"+language+" BLEU score: " + str(bleu))
+    print("en-"+language+" BLEU score: " + str(bleu))
--- examples/evaluation/wmt_test.sh
+++ examples/evaluation/wmt_test.sh
@@ -18,74 +18,419 @@ set -e
 
 BASE_DIR=$(pwd)
 
-OUTPUT_DIR=${OUTPUT_DIR:-$BASE_DIR/wmt14}
+# Clone Moses
+  if [ ! -d "${BASE_DIR}/mosesdecoder" ]; then
+    echo "Cloning moses for data processing"
+    git clone https://github.com/moses-smt/mosesdecoder.git "${BASE_DIR}/mosesdecoder"
+  fi
+
+
+
+
+
+#
+#-------------------------------------------------------WMT 2014-----------------------------------------------------
+#
+OUTPUT_DIR=${BASE_DIR}"/wmt14"
 
 OUTPUT_DIR_DATA="${OUTPUT_DIR}/data"
 
 mkdir -p $OUTPUT_DIR_DATA
 
 
-echo "Downloading test sets"
-wget -nc -nv -O  ${OUTPUT_DIR_DATA}/test.tgz https://www.statmt.org/wmt14/test-filtered.tgz
+  echo "Downloading test sets"
+  wget -nc -nv -O  ${OUTPUT_DIR_DATA}/test.tgz https://www.statmt.org/wmt14/test-filtered.tgz
 
-# Extract everything
-echo "Extracting all files..."
-mkdir -p "${OUTPUT_DIR_DATA}/test"
-tar -xvzf "${OUTPUT_DIR_DATA}/test.tgz" -C "${OUTPUT_DIR_DATA}/test"
+  if test -f "${OUTPUT_DIR_DATA}/test.tgz"; then
+    echo "${OUTPUT_DIR_DATA}/test.tgz downloaded."
+  else
+    echo "Not found URL https://www.statmt.org/wmt14/test.tgz."
+    exit
+  fi
 
-# Clone Moses
-if [ ! -d "${OUTPUT_DIR}/mosesdecoder" ]; then
-  echo "Cloning moses for data processing"
-  git clone https://github.com/moses-smt/mosesdecoder.git "${OUTPUT_DIR}/mosesdecoder"
-fi
-
-mkdir ${OUTPUT_DIR_DATA}/de_en
-mkdir ${OUTPUT_DIR_DATA}/fr_en
-mkdir ${OUTPUT_DIR_DATA}/de_en/test
-mkdir ${OUTPUT_DIR_DATA}/fr_en/test
-
-# Convert SGM files
-# Convert newstest2014 data into raw text format
-${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
-  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-src.de.sgm \
-  > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.src.de
-${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
-  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-ref.en.sgm \
-  > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.ref.en
-${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
-  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-src.en.sgm \
-  > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.src.en
-${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
-  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-ref.de.sgm \
-  > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.ref.de
+  # Extract everything
+  echo "Extracting all files..."
+  mkdir -p "${OUTPUT_DIR_DATA}/test"
+  tar -xvzf "${OUTPUT_DIR_DATA}/test.tgz" -C "${OUTPUT_DIR_DATA}/test"
 
 
-mkdir ${OUTPUT_DIR}/de_en
-mkdir ${OUTPUT_DIR}/fr_en
-# Copy dev/test data to output dir
-cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.de ${OUTPUT_DIR}/de_en
-cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.en ${OUTPUT_DIR}/de_en
+  mkdir ${OUTPUT_DIR_DATA}/de_en
+  mkdir ${OUTPUT_DIR_DATA}/fr_en
+  mkdir ${OUTPUT_DIR_DATA}/ru_en
+  mkdir ${OUTPUT_DIR_DATA}/de_en/test
+  mkdir ${OUTPUT_DIR_DATA}/fr_en/test
+  mkdir ${OUTPUT_DIR_DATA}/ru_en/test
+  mkdir ${OUTPUT_DIR}/de_en
+  mkdir ${OUTPUT_DIR}/fr_en
+  mkdir ${OUTPUT_DIR}/ru_en
+
+  # Convert SGM files
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-src.de.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.src.de
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-deen-ref.de.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2014.ref.de
 
 
-# Convert newstest2014 data into raw text format
-${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
-  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-src.fr.sgm \
-  > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.src.fr
-${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
-  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-ref.en.sgm \
-  > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.ref.en
-${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
-  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-src.en.sgm \
-  > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.src.en
-${OUTPUT_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
-  < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-ref.fr.sgm \
-  > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.ref.fr
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.de ${OUTPUT_DIR}/de_en
+  cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.en ${OUTPUT_DIR}/de_en
 
 
-# Copy dev/test data to output dir
-cp ${OUTPUT_DIR_DATA}/fr_en/test/newstest20*.fr ${OUTPUT_DIR}/fr_en
-cp ${OUTPUT_DIR_DATA}/fr_en/test/newstest20*.en ${OUTPUT_DIR}/fr_en
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-src.fr.sgm \
+    > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.src.fr
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-fren-ref.fr.sgm \
+    > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2014.ref.fr
 
-rm -rf ${OUTPUT_DIR_DATA}/test
-rm -f ${OUTPUT_DIR_DATA}/test.tgz
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/fr_en/test/newstest20*.fr ${OUTPUT_DIR}/fr_en
+  cp ${OUTPUT_DIR_DATA}/fr_en/test/newstest20*.en ${OUTPUT_DIR}/fr_en
+
+
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-ruen-src.ru.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2014.src.ru
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-ruen-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2014.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-ruen-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2014.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2014-ruen-ref.ru.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2014.ref.ru
+
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/ru_en/test/newstest20*.ru ${OUTPUT_DIR}/ru_en
+  cp ${OUTPUT_DIR_DATA}/ru_en/test/newstest20*.en ${OUTPUT_DIR}/ru_en
+
+  rm -rf ${OUTPUT_DIR_DATA}/test
+  rm -f ${OUTPUT_DIR_DATA}/test.tgz
+
+
+
+
+
+
+
+
+#
+#-------------------------------------------------------WMT 2015-----------------------------------------------------
+#
+  OUTPUT_DIR=${BASE_DIR}"/wmt15"
+
+  OUTPUT_DIR_DATA="${OUTPUT_DIR}/data"
+
+  mkdir -p $OUTPUT_DIR_DATA
+
+
+  echo "Downloading test sets"
+  wget -nc -nv -O  ${OUTPUT_DIR_DATA}/test.tgz https://www.statmt.org/wmt15/test.tgz
+  size=$(stat --printf="%s" ${OUTPUT_DIR_DATA}/test.tgz)
+  if [ $size -eq 0 ]; then
+    rm -f ${OUTPUT_DIR_DATA}/test.tgz
+  fi
+
+  if test -f "${OUTPUT_DIR_DATA}/test.tgz"; then
+    echo "${OUTPUT_DIR_DATA}/test.tgz downloaded."
+  else
+    echo "Not found URL https://www.statmt.org/wmt15/test.tgz."
+    exit
+  fi
+
+  # Extract everything
+  echo "Extracting all files..."
+  mkdir -p "${OUTPUT_DIR_DATA}/test"
+  tar -xvzf "${OUTPUT_DIR_DATA}/test.tgz" -C "${OUTPUT_DIR_DATA}/test"
+
+
+  mkdir ${OUTPUT_DIR_DATA}/de_en
+  mkdir ${OUTPUT_DIR_DATA}/fr_en
+  mkdir ${OUTPUT_DIR_DATA}/ru_en
+  mkdir ${OUTPUT_DIR_DATA}/de_en/test
+  mkdir ${OUTPUT_DIR_DATA}/fr_en/test
+  mkdir ${OUTPUT_DIR_DATA}/ru_en/test
+  mkdir ${OUTPUT_DIR}/de_en
+  mkdir ${OUTPUT_DIR}/fr_en
+  mkdir ${OUTPUT_DIR}/ru_en
+
+  # Convert SGM files
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2015-deen-src.de.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2015.src.de
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2015-deen-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2015.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2015-ende-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2015.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2015-ende-ref.de.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2015.ref.de
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.de ${OUTPUT_DIR}/de_en
+  cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.en ${OUTPUT_DIR}/de_en
+
+
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newsdiscusstest2015-fren-src.fr.sgm \
+    > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2015.src.fr
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newsdiscusstest2015-fren-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2015.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newsdiscusstest2015-enfr-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2015.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newsdiscusstest2015-enfr-ref.fr.sgm \
+    > ${OUTPUT_DIR_DATA}/fr_en/test/newstest2015.ref.fr
+
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/fr_en/test/newstest20*.fr ${OUTPUT_DIR}/fr_en
+  cp ${OUTPUT_DIR_DATA}/fr_en/test/newstest20*.en ${OUTPUT_DIR}/fr_en
+
+
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2015-ruen-src.ru.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2015.src.ru
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2015-ruen-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2015.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2015-enru-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2015.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2015-enru-ref.ru.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2015.ref.ru
+
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/ru_en/test/newstest20*.ru ${OUTPUT_DIR}/ru_en
+  cp ${OUTPUT_DIR_DATA}/ru_en/test/newstest20*.en ${OUTPUT_DIR}/ru_en
+
+  rm -rf ${OUTPUT_DIR_DATA}/test
+  rm -f ${OUTPUT_DIR_DATA}/test.tgz
+
+
+
+
+
+
+
+#
+#-------------------------------------------------------WMT 2016-----------------------------------------------------
+#
+  OUTPUT_DIR=${BASE_DIR}"/wmt16"
+
+  OUTPUT_DIR_DATA="${OUTPUT_DIR}/data"
+
+  mkdir -p $OUTPUT_DIR_DATA
+
+
+  echo "Downloading test sets"
+  wget -nc -nv -O  ${OUTPUT_DIR_DATA}/test.tgz http://data.statmt.org/wmt16/translation-task/test.tgz
+  size=$(stat --printf="%s" ${OUTPUT_DIR_DATA}/test.tgz)
+  if [ $size -eq 0 ]; then
+    rm -f ${OUTPUT_DIR_DATA}/test.tgz
+  fi
+
+  if test -f "${OUTPUT_DIR_DATA}/test.tgz"; then
+    echo "${OUTPUT_DIR_DATA}/test.tgz downloaded."
+  else
+    echo "Not found URL http://data.statmt.org/wmt16/translation-task/test.tgz."
+    exit
+  fi
+
+  # Extract everything
+  echo "Extracting all files..."
+  mkdir -p "${OUTPUT_DIR_DATA}/test"
+  tar -xvzf "${OUTPUT_DIR_DATA}/test.tgz" -C "${OUTPUT_DIR_DATA}/test"
+
+
+  mkdir ${OUTPUT_DIR_DATA}/de_en
+  mkdir ${OUTPUT_DIR_DATA}/ru_en
+  mkdir ${OUTPUT_DIR_DATA}/de_en/test
+  mkdir ${OUTPUT_DIR_DATA}/ru_en/test
+  mkdir ${OUTPUT_DIR}/de_en
+  mkdir ${OUTPUT_DIR}/ru_en
+
+  # Convert SGM files
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2016-deen-src.de.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2016.src.de
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2016-deen-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2016.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2016-ende-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2016.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2016-ende-ref.de.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2016.ref.de
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.de ${OUTPUT_DIR}/de_en
+  cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.en ${OUTPUT_DIR}/de_en
+
+
+  # Convert SGM files
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2016-ruen-src.ru.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2016.src.ru
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2016-ruen-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2016.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2016-enru-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2016.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2016-enru-ref.ru.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2016.ref.ru
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/ru_en/test/newstest20*.ru ${OUTPUT_DIR}/ru_en
+  cp ${OUTPUT_DIR_DATA}/ru_en/test/newstest20*.en ${OUTPUT_DIR}/ru_en
+
+  rm -rf ${OUTPUT_DIR_DATA}/test
+  rm -f ${OUTPUT_DIR_DATA}/test.tgz
+
+
+
+
+
+
+
+
+
+#
+#-------------------------------------------------------WMT 2017-----------------------------------------------------
+#
+  OUTPUT_DIR=${BASE_DIR}"/wmt17"
+
+  OUTPUT_DIR_DATA="${OUTPUT_DIR}/data"
+
+  mkdir -p $OUTPUT_DIR_DATA
+
+
+  echo "Downloading test sets"
+  wget -nc -nv -O  ${OUTPUT_DIR_DATA}/test.tgz http://data.statmt.org/wmt17/translation-task/test.tgz
+  size=$(stat --printf="%s" ${OUTPUT_DIR_DATA}/test.tgz)
+  if [ $size -eq 0 ]; then
+    rm -f ${OUTPUT_DIR_DATA}/test.tgz
+  fi
+
+  if test -f "${OUTPUT_DIR_DATA}/test.tgz"; then
+    echo "${OUTPUT_DIR_DATA}/test.tgz downloaded."
+  else
+    echo "Not found URL http://data.statmt.org/wmt17/translation-task/test.tgz."
+    exit
+  fi
+
+  # Extract everything
+  echo "Extracting all files..."
+  mkdir -p "${OUTPUT_DIR_DATA}/test"
+  tar -xvzf "${OUTPUT_DIR_DATA}/test.tgz" -C "${OUTPUT_DIR_DATA}/test"
+
+
+  mkdir ${OUTPUT_DIR_DATA}/de_en
+  mkdir ${OUTPUT_DIR_DATA}/ru_en
+  mkdir ${OUTPUT_DIR_DATA}/zh_en
+  mkdir ${OUTPUT_DIR_DATA}/de_en/test
+  mkdir ${OUTPUT_DIR_DATA}/ru_en/test
+  mkdir ${OUTPUT_DIR_DATA}/zh_en/test
+  mkdir ${OUTPUT_DIR}/de_en
+  mkdir ${OUTPUT_DIR}/ru_en
+  mkdir ${OUTPUT_DIR}/zh_en
+
+  # Convert SGM files
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-deen-src.de.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2017.src.de
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-deen-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2017.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-ende-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2017.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-ende-ref.de.sgm \
+    > ${OUTPUT_DIR_DATA}/de_en/test/newstest2017.ref.de
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.de ${OUTPUT_DIR}/de_en
+  cp ${OUTPUT_DIR_DATA}/de_en/test/newstest20*.en ${OUTPUT_DIR}/de_en
+
+
+  # Convert SGM files
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-ruen-src.ru.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2017.src.ru
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-ruen-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2017.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-enru-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2017.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-enru-ref.ru.sgm \
+    > ${OUTPUT_DIR_DATA}/ru_en/test/newstest2017.ref.ru
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/ru_en/test/newstest20*.ru ${OUTPUT_DIR}/ru_en
+  cp ${OUTPUT_DIR_DATA}/ru_en/test/newstest20*.en ${OUTPUT_DIR}/ru_en
+
+
+  # Convert SGM files
+  # Convert newstest2014 data into raw text format
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-zhen-src.zh.sgm \
+    > ${OUTPUT_DIR_DATA}/zh_en/test/newstest2017.src.zh
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-zhen-ref.en.sgm \
+    > ${OUTPUT_DIR_DATA}/zh_en/test/newstest2017.ref.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-enzh-src.en.sgm \
+    > ${OUTPUT_DIR_DATA}/zh_en/test/newstest2017.src.en
+  ${BASE_DIR}/mosesdecoder/scripts/ems/support/input-from-sgm.perl \
+    < ${OUTPUT_DIR_DATA}/test/test/newstest2017-enzh-ref.zh.sgm \
+    > ${OUTPUT_DIR_DATA}/zh_en/test/newstest2017.ref.zh
+
+  # Copy dev/test data to output dir
+  cp ${OUTPUT_DIR_DATA}/zh_en/test/newstest20*.zh ${OUTPUT_DIR}/zh_en
+  cp ${OUTPUT_DIR_DATA}/zh_en/test/newstest20*.en ${OUTPUT_DIR}/zh_en
+
+  rm -rf ${OUTPUT_DIR_DATA}/test
+  rm -f ${OUTPUT_DIR_DATA}/test.tgz
+
+
+rm -r\f "${BASE_DIR}/mosesdecoder"
 echo "All done."
